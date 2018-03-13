26d1dbf2bfe5d213ff2acfb66f9a2d6889d54e17
Kelly Packer <kelly@kellypacker.com>
2018-03-13 15:51:16 -0400

    Remove all files except readme for master branch

--- assets/js/ui/externalLinks.js
+++
@@ -1,5 +0,0 @@
-// OPEN .PDF'S AND EXTERNAL LINKS IN A NEW WINDOW
-export function buildExternal() {
-    $('a[href^="http:"], a[href^="https:"]').not('[href*="'+document.domain+'"]').attr('target','_blank');
-    $('a[href$=".pdf"]').attr('target', '_blank');
-}
