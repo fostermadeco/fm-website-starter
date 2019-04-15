15208a6f9ac46494005cea082b186d5437baba9e
Kelly Packer <kelly@kellypacker.com>
2019-04-15 10:32:05 -0600

    Formatting

--- resources/js/ui/ui.js
+++ resources/js/ui/ui.js
@@ -1,6 +1,8 @@
 // OPEN .PDF'S AND EXTERNAL LINKS IN A NEW WINDOW
 const openExternalInNewWindow = () => {
-    $('a[href^="http:"], a[href^="https:"]').not(`[href*="${document.domain}"]`).attr('target', '_blank');
+    $('a[href^="http:"], a[href^="https:"]')
+        .not(`[href*="${document.domain}"]`)
+        .attr('target', '_blank');
     $('a[href$=".pdf"]').attr('target', '_blank');
 };
 
